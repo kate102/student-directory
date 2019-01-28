e4acf0784e1df553869950aaa06bb36a96b05e56
kate102 <kate.morris.new@gmail.com>
2019-01-28 13:13:23 +0000

    Added a student_count variable

--- directory.rb
+++ directory.rb
@@ -1,3 +1,5 @@
+student_count = 11
+
 puts "The students of Villians Academy"
 puts "---------------"
 puts "Dr. Hannibal Lecter"
@@ -13,7 +15,4 @@ puts "Joffrey Baratheon"
 puts "Norman Bates"
 
 # Now we add a print statement to count the number of students
-print "Overall we have "
-# It is importnant that print() doesn't add a new line
-print 11
-print " great students!"
+print "Overall we have #{student_count} great students!"
