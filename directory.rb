aa57d1eec4d8fdbbe89fe8d7a36cad41c027729b
kate102 <kate.morris.new@gmail.com>
2019-02-01 09:19:36 +0000

    reduced the line count by two by using interpolation

--- directory.rb
+++ directory.rb
@@ -1,3 +1,5 @@
+student_count = 11
+
 # First we print the list of students
 puts "The students of Villains Academy"
 puts "-------------"
@@ -14,7 +16,4 @@ puts "Joffrey Baratheon"
 puts "Norman Bates"
 
 # Finally, we print the total number of students
-print "Overall, we have "
-# it is important that print() doesn’t add new line characters
-print 11
-puts " great students"
+puts "Overall, we have #{student_count} great students"
