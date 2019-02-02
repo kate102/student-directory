d2107c3bee757ec1835eeaaf6101e02c7fc833ce
kate102 <kate.morris.new@gmail.com>
2019-02-02 17:15:12 +0000

    Completed Step 13

--- directory.rb
+++ directory.rb
@@ -1,106 +1,148 @@
-@students = [] # an empty array accessible to all methods
+require 'date'
 
-def print_menu
-  puts "1. Input the students"
-  puts "2. Show the students"
-  puts "3. Save the list to students.csv"
-  puts "4. Load the list from students.csv"
-  puts "9. Exit" # 9 because we'll be adding more items
-end
+# ------- Initialise Global Variables --
+
+@students = []
+@file_name = ARGV.first
+@default_file_name = "students.csv"
+
+# ------- Main Menu Loop --------
 
 def interactive_menu
+  ARGV.clear
   loop do
     print_menu
-    process(STDIN.gets.chomp)
-  end
+    process(gets.chomp)
+  end # loop
 end
+
+# ------- Processing Methods ----
 
 def process(selection)
   case selection
-  when "1"
-    input_students
-  when "2"
-    show_students
-  when "3"
-    save_students
-  when "4"
-    load_students
-  when "9"
-    exit # this will cause the program to terminate
-  else
-    puts "I don't know what you meant, try again"
-  end
+    when "1"
+      @students = input_students
+    when "2"
+      print_students           
+    when "3"
+      save_students           
+    when "4"
+      try_load_students           
+    when "9"
+      exit
+    else
+      puts "I don't know what you mean"
+  end # case
 end
+
+# ------- Get and Store Data Methods --
 
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
   # get the first name
-  name = STDIN.gets.chomp
+  name = gets.chomp
   # while the name is not empty, repeat this code
   while !name.empty? do
     # add the student hash to the array
     @students << {name: name, cohort: :november}
     puts "Now we have #{@students.count} students"
     # get another name from the user
-    name = STDIN.gets.chomp
+    name = gets.chomp
   end
-end
-  
-def show_students
-  print_header
-  print_student_list
-  print_footer
+  # return the array of students
+  return @students
 end
 
-def print_header
-  puts "The students of Villains Academy"
-  puts "-------------"
-end
-
-def print_student_list
-  @students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+def get_filename
+  if @file_name.nil? 
+    return @default_file_name
+  else
+     return @file_name
   end
-end
-
-def print_footer
-  puts "Overall, we have #{@students.count} great students"
-end
+end 
 
 def save_students
-  # open the file for writing
-  file = File.open("students.csv", "w")
-  # iterate over the array of students
+  filename = get_filename
+  puts "File name is [#{filename}]"
+  # Open the file for writing
+  file = File.open(filename,"w")
   @students.each do |student|
     student_data = [student[:name], student[:cohort]]
     csv_line = student_data.join(",")
     file.puts csv_line
   end
   file.close
+  puts "Student Details Written To File #{filename}"
 end
 
+
 def load_students(filename = "students.csv")
-  file = File.open(filename, "r")
+  @students = [] 
+  # Open the file for writing
+  file = File.open(filename,"r")
   file.readlines.each do |line|
     name, cohort = line.chomp.split(',')
+    # Note that this adds the students in the file to the local student list
+    # Not sure that we would want to do this IRL
     @students << {name: name, cohort: cohort.to_sym}
   end
   file.close
+  puts "Student Details Read from File #{filename}"
 end
 
 def try_load_students
-  filename = ARGV.first # first argument from the command line
-  return if filename.nil? # get out of the method if it isn't given
-    if File.exists?(filename) # if it exists
-      load_students(filename)
-      puts "Loaded #{@students.count} from #{filename}"
-    else # if it doesn't exist
-      puts "Sorry, #{filename} doesn't exist."
-      exit # quit the program
-    end
+  filename = get_filename
+  # Open the file for writing
+  if File.exists?(filename) # Check it exists
+    load_students(filename)
+    puts "Loaded #{@students.count} from #{filename}"
+  else
+    puts "Sorry but file, #{filename}, does not exist"
+    exit
+  end
 end
-                                                       
-interactive_menu
-try_load_students
+    
+# ------- Print Methods ---------
 
+def print_menu
+  s = "-" * 50
+  puts s.center(50)
+  puts "Menu".center(50)
+  puts s.center(50)
+  puts "1. Input the students details"
+  puts "2. Show the students details"
+  puts "3. Save the students details to file"
+  puts "4. Load the students details from a file"
+  puts "9. Exit"
+end
+
+def print_students
+  print_header
+  print_details
+  print_footer
+end
+
+def print_header
+  puts "The students of my cohort at Makers Academy".center(50)
+  s = "-" * 50
+  puts s.center(50)
+end
+
+def print_details
+  @students.each do |student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  end
+end
+
+def print_footer
+  n = @students.length
+  if n > 0
+    puts "Overall, we have #{n} great students"
+  else
+    puts "Overall, we have no students"
+  end
+end
+
+# -----------------------------
+interactive_menu
