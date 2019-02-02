6c7c9dc227a83b4628ee633d9fce5660815f1d0a
kate102 <kate.morris.new@gmail.com>
2019-02-02 16:30:42 +0000

    Completed all of step 8

--- directory.rb
+++ directory.rb
@@ -1,3 +1,4 @@
+### Step 8 - Q11 Note that this is doe outside this program
 # The list of possible co-horts, Unknown is the default
 @cohorts = ["Unknown","January","February","March","April","May","June","July","August","September","October","November","December"]
 
@@ -17,6 +18,7 @@
       :Unknown =>[]
   }
 
+### Step 8 - Q8 Group by cohort
 def print_cohort_grouping(students)
   students.each do |student|
     @cohort_hash[student[:cohort]] << student[:name]
@@ -32,10 +34,13 @@ def input_students_data
   puts "Please enter the details of the students"
   puts "To finish, just hit return twice"
   students = []
+  ### Step 8 - Q9 Output student/s depending on number of students
   s_string = "student"
   puts "Please enter the name of the student followed by their cohort"
-  name = gets.chomp
-  # text3 = text2[0,text2.length-1]
+  ### Step 8 - Q10 Use something instead of chomp
+  name = gets.sub("\n","") 
+  ### Step 8 - Q4 Use while instead of each
+  ### Step 8 - Q7 Get the cohort, convert from symbol and deal with invalid or empty strings and provide a default
   while !name.empty? do
     cohort = gets.chomp.to_sym
     while @cohorts.find_index(cohort.to_s) == nil && !cohort.empty? do
@@ -45,6 +50,7 @@ def input_students_data
     if cohort.empty?
       cohort = "Unknown".to_sym
     end
+    ### Step 8 - Q5 Input additional details 
     puts "Please enter their height"
     height = gets.chomp
     puts "and their country of birth  "
@@ -61,9 +67,6 @@ def input_students_data
   return students
 end
   
-
-
-
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, hit return twice"
@@ -80,15 +83,44 @@ end
 def print_header
   header_string1 = "The Students of Villians Academy"
   header_string2 = "-" * header_string1.length
+  ### Step 8 - Q6 Use center to improve the display (Note that I have used ljust in print details to tabulate)
   puts header_string1.center(150)
   puts header_string2.center(150,"-")
 end
 
+### Step 8 - Q1 Print number
 def print_details(students)
-  students.each_with_index do |student,i|
-    puts "#{i+1}: #{student[:name]}".ljust(20)  +"cohort - #{student[:cohort]}".to_s.ljust(20) + "Height is #{student[:height]}".ljust(20) + "CoB is #{student[:cob]}".ljust(20) + " and hobbies are #{student[:hobbies]}"
+  ### Step 8 - Q12 Print students only if the list is not empty
+  if !students.empty?
+    students.each_with_index do |student,i|
+      puts "#{i+1}: #{student[:name]}".ljust(20)  +"cohort - #{student[:cohort]}".to_s.ljust(20) + "Height is #{student[:height]}".ljust(20) + "CoB is #{student[:cob]}".ljust(20) + " and hobbies are #{student[:hobbies]}"
+    end
+  else
+    puts "There are no students to show"
   end
 end
+
+### Step 8 - Q2 Print students whose names begin with a certain letter
+def print_find_name_with_letter(students)
+  puts " please enter letter"
+  letter = gets.chomp
+  students.each_with_index do |student,i|
+    if student[:name][0] == letter
+      puts "#{i+1}: #{student[:name]} (cohort - #{student[:cohort]} )"
+    end
+  end
+end
+
+### Step 8 - Q3 Print students whose names are shorter than 12 characters
+def print_short_name(students)
+  puts "\n The list of students with names shorter than 12 is :"
+  students.each_with_index do |student,i|
+    if student[:name].length < 12
+      puts "#{i+1}: #{student[:name]} (cohort - #{student[:cohort]} )"
+    end
+  end
+end
+
 
 def print_footer(names)
   puts "Overall, we have #{names.count} great students"
